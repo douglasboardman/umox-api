6a698e632a12886709a72f9012bcd8af60cc355e
Douglas Boardman <douglas.boardman@gmail.com>
2022-11-18 15:35:01 -0300

    Instalando dependências

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
+
+app.listen(port, ()=>{
+    console.log('Servidor rodando');
+});

86f856eef689327f2076e5ed47819e027537ab61
Douglas Boardman <douglas.boardman@gmail.com>
2022-11-18 16:06:06 -0300

    Atualizações no index.js

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const port = 3000;
 var path = require('path');
 const app = express();
 
+app.use(session({secret: 'kdhe74jfollsd8kefjsiudf34jd'}));
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res)=>{
+    res.render('index');
+});
 
 app.listen(port, ()=>{
     console.log('Servidor rodando');

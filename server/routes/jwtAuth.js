cd894c72bc0ca88b68c4f7b3d0ab09f7d6af2a9d
Douglas Boardman <46564691+douglasboardman@users.noreply.github.com>
2022-11-20 14:25:23 -0300

    Commit

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -4,11 +4,11 @@ const pool = require("../db");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
 
-// registering
+// router do form de registro de usuário
 
 router.post("/register", async (req,res)=>{
     try {
-        // 1. destructure do Login > req.body (nome, email, senha)
+        // 1. destructure do formulário de registro > req.body (nome, email, senha)
 
         const {nome, email, senha} = req.body;
 
@@ -33,7 +33,7 @@ router.post("/register", async (req,res)=>{
             [nome, email, bcryptSenha]
         );
 
-        // 5. gerar token jwt
+        // 5. atribuir token jwt
 
         const token = jwtGenerator(novoUsuario.rows[0].id_usuario);
         res.json({token});
@@ -43,6 +43,44 @@ router.post("/register", async (req,res)=>{
         res.status(500).send("Erro no Servidor");
     }
 });
+
+// router do form de login
+
+router.post("/login", async (req, res) => {
+    try {
+        // 1. destructure do formulário de login > req.body (email, senha)
+        const { email, senha } = req.body;
+
+        // 2. confere se o usuário não existe (se não existe dispara um erro)
+        
+        const usuario = await pool.query(
+            "SELECT * FROM usuarios WHERE email_usuario = $1",
+            [email]
+        );
+
+        if (usuario.rows.length === 0) {
+            return res.status(401).send("Senha ou usuário incorretos");
+        }
+
+        // 3. checa se a senha informada é a mesma registrada no banco
+        
+        const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha_usuario);
+
+        if (!senhaValida) {
+            return res.status(401).json("Usuário ou senha incorretos");
+        }
+
+        // 4. atribuir token jwt
+
+        const token = jwtGenerator(usuario.rows[0].id_usuario);
+
+        res.json({token});
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Erro no Servidor");
+    }
+})
 
 
 module.exports = router;
